3318be2be0677b479baf93680f93000244ed2c6e
direboar <direboar@example.com>
2023-08-22 15:17:07 +0000

    daisyUI

--- daisyUI/src/routes/+layout.svelte
+++ daisyUI/src/routes/+layout.svelte
@@ -1,5 +1,168 @@
 <script>
+	import { onMount } from 'svelte';
 	import '../app.css';
+	onMount(() => {
+		document.documentElement.setAttribute('data-theme', 'dark');
+	});
 </script>
 
-<slot />
+<div class="navbar bg-base-100">
+	<div class="flex-auto">
+		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+
+		<!-- search icon w-9 h-9でサイズ制御-->
+		<button class="btn">
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				fill="none"
+				viewBox="0 0 24 24"
+				stroke-width="1.5"
+				stroke="currentColor"
+				class="w-8 h-8"
+			>
+				<path
+					stroke-linecap="round"
+					stroke-linejoin="round"
+					d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z"
+				/>
+			</svg>
+		</button>
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-96" />
+		</div>
+	</div>
+
+	<div class="flex-auto gap-2">
+		<button class="btn">
+			<!-- Discord -->
+			<svg
+				class="h-5 w-5"
+				fill="currentColor"
+				viewbox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+			>
+				<path
+					d="M19.54 0c1.356 0 2.46 1.104 2.46 2.472v21.528l-2.58-2.28-1.452-1.344-1.536-1.428.636 2.22h-13.608c-1.356 0-2.46-1.104-2.46-2.472v-16.224c0-1.368 1.104-2.472 2.46-2.472h16.08zm-4.632 15.672c2.652-.084 3.672-1.824 3.672-1.824 0-3.864-1.728-6.996-1.728-6.996-1.728-1.296-3.372-1.26-3.372-1.26l-.168.192c2.04.624 2.988 1.524 2.988 1.524-1.248-.684-2.472-1.02-3.612-1.152-.864-.096-1.692-.072-2.424.024l-.204.024c-.42.036-1.44.192-2.724.756-.444.204-.708.348-.708.348s.996-.948 3.156-1.572l-.12-.144s-1.644-.036-3.372 1.26c0 0-1.728 3.132-1.728 6.996 0 0 1.008 1.74 3.66 1.824 0 0 .444-.54.804-.996-1.524-.456-2.1-1.416-2.1-1.416l.336.204.048.036.047.027.014.006.047.027c.3.168.6.3.876.408.492.192 1.08.384 1.764.516.9.168 1.956.228 3.108.012.564-.096 1.14-.264 1.74-.516.42-.156.888-.384 1.38-.708 0 0-.6.984-2.172 1.428.36.456.792.972.792.972zm-5.58-5.604c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332.012-.732-.54-1.332-1.224-1.332zm4.38 0c-.684 0-1.224.6-1.224 1.332 0 .732.552 1.332 1.224 1.332.684 0 1.224-.6 1.224-1.332 0-.732-.54-1.332-1.224-1.332z"
+				/>
+			</svg>
+		</button>
+		<button class="btn">
+			<!-- Facebook -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="currentColor"
+				viewBox="0 0 24 24"
+			>
+				<path
+					d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"
+				/>
+			</svg>
+		</button>
+		<button class="btn">
+			<!-- Messenger -->
+			<svg
+				class="h-5 w-5"
+				fill="currentColor"
+				viewbox="0 0 24 24"
+				xmlns="http://www.w3.org/2000/svg"
+				fill-rule="evenodd"
+				clip-rule="evenodd"
+			>
+				<path
+					d="M12 0c-6.627 0-12 4.975-12 11.111 0 3.497 1.745 6.616 4.472 8.652v4.237l4.086-2.242c1.09.301 2.246.464 3.442.464 6.627 0 12-4.974 12-11.111 0-6.136-5.373-11.111-12-11.111zm1.193 14.963l-3.056-3.259-5.963 3.259 6.559-6.963 3.13 3.259 5.889-3.259-6.559 6.963z"
+				/>
+			</svg>
+		</button>
+		<button class="btn">
+			<!-- Twitter -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="currentColor"
+				viewBox="0 0 24 24"
+			>
+				<path
+					d="M24 4.557c-.883.392-1.832.656-2.828.775 1.017-.609 1.798-1.574 2.165-2.724-.951.564-2.005.974-3.127 1.195-.897-.957-2.178-1.555-3.594-1.555-3.179 0-5.515 2.966-4.797 6.045-4.091-.205-7.719-2.165-10.148-5.144-1.29 2.213-.669 5.108 1.523 6.574-.806-.026-1.566-.247-2.229-.616-.054 2.281 1.581 4.415 3.949 4.89-.693.188-1.452.232-2.224.084.626 1.956 2.444 3.379 4.6 3.419-2.07 1.623-4.678 2.348-7.29 2.04 2.179 1.397 4.768 2.212 7.548 2.212 9.142 0 14.307-7.721 13.995-14.646.962-.695 1.797-1.562 2.457-2.549z"
+				/>
+			</svg>
+		</button>
+		<button class="btn">
+			<!-- Google -->
+			<svg
+				xmlns="http://www.w3.org/2000/svg"
+				class="h-5 w-5"
+				fill="currentColor"
+				viewBox="0 0 24 24"
+			>
+				<path
+					d="M7 11v2.4h3.97c-.16 1.029-1.2 3.02-3.97 3.02-2.39 0-4.34-1.979-4.34-4.42 0-2.44 1.95-4.42 4.34-4.42 1.36 0 2.27.58 2.79 1.08l1.9-1.83c-1.22-1.14-2.8-1.83-4.69-1.83-3.87 0-7 3.13-7 7s3.13 7 7 7c4.04 0 6.721-2.84 6.721-6.84 0-.46-.051-.81-.111-1.16h-6.61zm0 0 17 2h-3v3h-2v-3h-3v-2h3v-3h2v3h3v2z"
+					fill-rule="evenodd"
+					clip-rule="evenodd"
+				/>
+			</svg>
+		</button>
+	</div>
+	<div class="flex-auto gap-2">
+		<div class="dropdown dropdown-end">
+			<label tabindex="0" class="btn btn-ghost"> minokuba </label>
+			<ul
+				tabindex="0"
+				class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+			>
+				<li>
+					<a class="justify-between">
+						Profile
+						<span class="badge">New</span>
+					</a>
+				</li>
+				<li><a>Settings</a></li>
+				<li><a>Logout</a></li>
+			</ul>
+		</div>
+	</div>
+
+	<!-- <div class="flex-auto">
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-96 md:w-auto" />
+		</div>
+	</div>
+	<div class="flex-auto">
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-96 md:w-auto" />
+		</div>
+	</div> -->
+</div>
+
+<!-- <div class="navbar bg-base-100">
+	<div class="flex-1">
+		<a class="btn btn-ghost normal-case text-xl">daisyUI</a>
+	</div>
+	<div class="flex-none gap-2">
+		<div class="form-control">
+			<input type="text" placeholder="Search" class="input input-bordered w-24 md:w-auto" />
+		</div>
+		<div class="dropdown dropdown-end">
+			<label tabindex="0" class="btn btn-ghost btn-circle avatar">
+				<div class="w-10 rounded-full">
+					<img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+				</div>
+			</label>
+			<ul
+				tabindex="0"
+				class="mt-3 z-[1] p-2 shadow menu menu-sm dropdown-content bg-base-100 rounded-box w-52"
+			>
+				<li>
+					<a class="justify-between">
+						Profile
+						<span class="badge">New</span>
+					</a>
+				</li>
+				<li><a>Settings</a></li>
+				<li><a>Logout</a></li>
+			</ul>
+		</div>
+	</div>
+</div> -->
